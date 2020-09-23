abd63d2bbe55c9c624d106a15ca2411795e3118b
brenoo2018 <thaynanbreno@gmail.com>
2020-09-23 17:19:00 -0300

    add logo

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+import logoImage from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <View>
       <Text>SignIn</Text>
+      <Image source={logoImage} />
     </View>
   );
 };
