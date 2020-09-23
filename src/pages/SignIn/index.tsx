ebf8f2bfca298e108ac5346a0ec5a47b9a7aab35
brenoo2018 <thaynanbreno@gmail.com>
2020-09-23 17:08:21 -0300

    configured navigation

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SignIn: React.FC = () => {
+  return (
+    <View>
+      <Text>SignIn</Text>
+    </View>
+  );
+};
+
+export default SignIn;
