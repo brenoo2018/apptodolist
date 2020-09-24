4ad8277790f84179563bcf59db231f9cca459180
brenoo2018 <thaynanbreno@gmail.com>
2020-09-24 19:06:17 -0300

    update readme

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.15:3333',
+  baseURL: 'http://ip_emulador_ou_do_smartphone_fisico:3333',
 });
+
+// baseURL: 'http://192.168.0.15:3333',
 
 export default api;
